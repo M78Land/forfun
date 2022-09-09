67aafcb0797b9b81b5675c92cfeadd1bd1f0a539
M78Land <109493744+M78Land@users.noreply.github.com>
2022-09-09 15:10:37 +0800

    Update remake.sh

--- remake.sh
+++ remake.sh
@@ -1,12 +1,3 @@
-#Set the environment
-
-docker container exec boss /bin/bash -c 'echo "cd /home/cmthome" | sudo tee /home/remake.sh'
-docker container exec boss /bin/bash -c 'echo "source setupCMT.sh" | sudo tee -a /home/remake.sh'
-docker container exec boss /bin/bash -c 'echo "cmt config && source setup.sh" | sudo tee  -a /home/remake.sh'
-docker container exec boss /bin/bash -c 'echo "cd /home/workarea/TestRelease/*/cmt" | sudo tee -a /home/remake.sh'
-docker container exec boss /bin/bash -c 'echo "cmt broadcast cmt config" | sudo tee -a /home/remake.sh'
-docker container exec boss /bin/bash -c 'echo "source setup.sh" | sudo tee -a /home/remake.sh'
-
 #Copy the package modified by you to the workarea inside the docker
 docker cp ./Analysis boss:/home/workarea/
 
@@ -16,7 +7,17 @@ docker container exec boss /bin/bash -c 'cp -r /cvmfs/bes3.ihep.ac.cn/bes3sw/Bos
 #modify the cxx
 docker container exec boss /bin/bash -c "sed -i '939a cout<<\\\"It is a vital success for jiangdi\\\"<<endl;' /home/workarea/Analysis/Physics/RhopiAlg/RhopiAlg-00-00-23/src/Rhopi.cxx"
 
-docker container exec boss /bin/bash -c 'echo "rm -rf /home/workarea/Analysis/Physics/RhopiAlg/RhopiAlg-00-00-23/x86_64-slc6-gcc46-opt" | sudo tee -a /home/remake.sh'
+#Set the environment
+
+docker container exec boss /bin/bash -c 'echo "cd /home/cmthome" | sudo tee /home/remake.sh'
+docker container exec boss /bin/bash -c 'echo "source setupCMT.sh" | sudo tee -a /home/remake.sh'
+docker container exec boss /bin/bash -c 'echo "cmt config && source setup.sh" | sudo tee  -a /home/remake.sh'
+docker container exec boss /bin/bash -c 'echo "cd /home/workarea/TestRelease/*/cmt" | sudo tee -a /home/remake.sh'
+docker container exec boss /bin/bash -c 'echo "cmt broadcast cmt config" | sudo tee -a /home/remake.sh'
+docker container exec boss /bin/bash -c 'echo "source setup.sh" | sudo tee -a /home/remake.sh'
+
+#make the modification
+#docker container exec boss /bin/bash -c 'echo "rm -rf /home/workarea/Analysis/Physics/RhopiAlg/RhopiAlg-00-00-23/x86_64-slc6-gcc46-opt" | sudo tee -a /home/remake.sh'
 docker container exec boss /bin/bash -c 'echo "cd /home/workarea/Analysis/Physics/RhopiAlg/RhopiAlg-00-00-23/cmt" | sudo tee -a /home/remake.sh'
 docker container exec boss /bin/bash -c 'echo "cmt config" | sudo tee -a /home/remake.sh'
 docker container exec boss /bin/bash -c 'echo "source setup.sh" | sudo tee -a /home/remake.sh'
